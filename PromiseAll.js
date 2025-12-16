1c3b5687ba15598281755fca1d9feb45d1a45151
DIVINEakisa <akisadivine11@gmail.com>
2025-12-16 19:17:51 +0200

    Add Promise.all implementation

---
+++ PromiseAll.js
@@ -0,0 +1,22 @@
+// Promise.all – Fail Fast Behavior
+
+// Create 3 Promises:
+
+// p1 resolves "User data" after 1s
+
+// p2 rejects "Auth failed" after 2s
+
+// p3 resolves "Settings loaded" after 3s
+
+// Task:
+
+// Use Promise.all() and explain:
+
+// What happens?
+
+// Does p3 still run?
+
+// Which value is returned?
+
+// 📌 Goal: Understand fail-fast behavior.
+function PromiseMethod(iterable) {}
