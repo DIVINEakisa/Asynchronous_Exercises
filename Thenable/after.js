73367715535d40469440cc74cbf59004c79b8394
DIVINEakisa <akisadivine11@gmail.com>
2026-01-07 17:41:23 +0200

    Add descriptiion in after.js

--- Thenable/after.js
+++ Thenable/after.js
@@ -1,3 +1,13 @@
+// Exercise 1: Basic .then()
+
+// Create a function that returns a Promise which:
+
+// Resolves after 2 seconds
+
+// Resolves with the message "Done"
+
+// Task:
+// Use .then() to print the message.
 function afterTwoSecond() {
   return {
     then(resolve, reject) {
