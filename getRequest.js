68234f53b7acc1fc6310ee0d295f274c7883b379
DIVINEakisa <akisadivine11@gmail.com>
2026-01-22 15:44:20 +0200

    HTTP GET with Promises Answer

--- getRequest.js
+++ getRequest.js
@@ -1,1 +1,6 @@
 // Write a JavaScript a function that makes an HTTP GET request and returns a Promise that resolves with the response data.
+function getData(){
+   return fetch("https://jsonplaceholder.typicode.com/posts").then(resp=>resp.json()).catch(err=>console.log(err));
+
+}
+getData().then(data=>console.log(data)).catch(err=>console.log(err));
