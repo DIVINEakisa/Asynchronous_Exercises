c89d6c69ebca8ef3aaae538766d7e43154f2d774
DIVINEakisa <akisadivine11@gmail.com>
2026-01-22 11:18:24 +0200

    Delay Callback Execution output

--- delay.js
+++ delay.js
@@ -1,3 +1,10 @@
-// 1. Delayed Callback Execution
-
 // Write a JavaScript function that takes a callback and invokes it after a delay of 2 second.
+function TakeCallback(cbk){
+    return setTimeout(()=>{
+       cbk();
+    },2000);
+
+}
+TakeCallback(()=>{
+ console.log("this is a call back function invoked after 2 sec");
+});
