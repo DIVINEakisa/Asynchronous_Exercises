72edb8b3e4eb77d2d135843911c8a7f6848c4415
DIVINEakisa <akisadivine11@gmail.com>
2026-01-28 17:13:16 +0200

    function that execute a given functio  repeatedly

--- setInterval.js
+++ setInterval.js
@@ -1,1 +1,2 @@
-// Write a JavaScript program to implement a function that executes a given function repeatedly at a fixed interval using 'setInterval()'.
+// Write a JavaScript program to implement a function 
+// that executes a given function repeatedly at a fixed interval using 'setInterval()'.
