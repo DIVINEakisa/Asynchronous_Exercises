8f070ce6af37a6b6cd187b086a2c20fa4f6ae0ef
DIVINEakisa <akisadivine11@gmail.com>
2026-01-28 17:27:36 +0200

    Output

--- setInterval.js
+++ setInterval.js
@@ -1,2 +1,14 @@
 // Write a JavaScript program to implement a function 
 // that executes a given function repeatedly at a fixed interval using 'setInterval()'.
+function repeatFunctionAtInterval(interval) {
+    return setInterval(()=>{
+        console.log("Function executed at interval of " + interval + " milliseconds");
+    }, interval);
+}   
+const intervalId = repeatFunctionAtInterval(2000);
+
+// To stop the interval after some time (for example, after 10 seconds)     
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("Interval cleared");
+}, 10000);
