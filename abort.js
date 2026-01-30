c201af024aa8b4392a27072433788f21577476a0
DIVINEakisa <akisadivine11@gmail.com>
2026-01-30 18:25:37 +0200

    Abort

--- abort.js
+++ abort.js
@@ -1,9 +1,18 @@
-// // Implement an abortable async function using `AbortController`, ensuring only the latest request result is used.
-// async function abortFun(url){
-//     let controller = new AbortController()
-//     let signal = {signal};
-//     let fet = await fetch(url,{signal});
-    
-    
-    
-// }
+// Implement an abortable async function using `AbortController`, ensuring only the latest request result is used.
+let controller = new AbortController();
+const signal = controller.signal;
+let changetoLatest = async () => {
+  try {
+    let url = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      signal,
+    });
+    let resp = await url.json();
+    console.log(resp);
+  } catch (err) {
+    if (err.name == "AbortMessage") {
+      console.error("It's Cancelled");
+    } else {
+      console.error("Something Went Wrong");
+    }
+  }
+};
