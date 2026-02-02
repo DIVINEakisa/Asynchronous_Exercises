e84035452ba6d6c8f03113bac4193d07ce8b40ca
DIVINEakisa <akisadivine11@gmail.com>
2026-02-02 14:19:52 +0200

    Add New folder that contains all the trial gate practical that we did

---
+++ trialGateQuestion/concurrently.js
@@ -0,0 +1,2 @@
+// This is the practical question I had given: Write a JavaScript function that fetches data from multiple APIs concurrently and returns a combined result using Promises and 'Promise.all()'.
+// https://jsonplaceholder.typicode.com/posts/1
