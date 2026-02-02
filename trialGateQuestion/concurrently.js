64611d66651cf0d74b9b26e6307b6819a61f0daa
DIVINEakisa <akisadivine11@gmail.com>
2026-02-02 15:04:48 +0200

    question it's to return the promise concurrently

--- trialGateQuestion/concurrently.js
+++ trialGateQuestion/concurrently.js
@@ -1,2 +1,19 @@
-// This is the practical question I had given: Write a JavaScript function that fetches data from multiple APIs concurrently and returns a combined result using Promises and 'Promise.all()'.
+// This is the practical question I had given:
+// Write a JavaScript function that fetches
+// data from multiple APIs concurrently and returns a combined result using Promises and 'Promise.all()'.
 // https://jsonplaceholder.typicode.com/posts/1
+
+async function fetchData() {
+  let urls = [
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2",
+    "https://jsonplaceholder.typicode.com/posts/3",
+  ];
+  let fet = urls.map((url) => {
+    return fetch(url).then((resp) => resp.json());
+  });
+  return Promise.all(fet);
+}
+fetchData()
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
