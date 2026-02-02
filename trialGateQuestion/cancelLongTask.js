2565c7a1b437adac9b8a6500e2e89f81b75c1bd7
DIVINEakisa <akisadivine11@gmail.com>
2026-02-02 16:54:05 +0200

    Function That cancel the request if the task long to be proceed

--- trialGateQuestion/cancelLongTask.js
+++ trialGateQuestion/cancelLongTask.js
@@ -1,2 +1,24 @@
 // Write a JavaScript function that fetches data from an API and
 // cancels the request if it takes longer than a specified time.
+
+async function fetchData() {
+  let controller = new AbortController();
+  let signal = controller.signal;
+  let timeOut = setTimeout(() => {
+    controller.abort();
+  }, 500);
+  try {
+    let url = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      signal,
+    });
+    let resp = await url.json();
+    clearTimeout(timeOut);
+    return resp;
+  } catch (err) {
+    if (err.name === "AbortError") {
+      console.log("Request Time Out");
+    }
+    throw err;
+  }
+}
+fetchData().then((data) => console.log(data));
